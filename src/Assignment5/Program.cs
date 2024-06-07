c8a25e15492f7474345b7aab1f85411c9c96876e
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2024-06-07 14:45:06 +0600

    file added.

---
+++ src/Assignment5/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Assignment5
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] twoDArray = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+   
+            Console.WriteLine("Two-dimensional array:");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(twoDArray[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+}
